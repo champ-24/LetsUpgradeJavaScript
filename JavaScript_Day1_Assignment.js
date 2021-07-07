f1ba82d1bd57d8bdecc81a96c16633b90a4df5b0
champ-24 <62603085+champ-24@users.noreply.github.com>
2021-07-07 09:38:59 +0530

    JavaScript Day 1 Assignment

---
+++ JavaScript_Day1_Assignment.js
@@ -0,0 +1,15 @@
+//Javascript Day 1 Assignment
+//Suyog Jagtap
+console.log(" * * * * * ")
+let v = "";
+
+  for(var j = 0;j < 5;j++){
+    v += " *" 
+  }
+
+console.log(v)
+
+
+//Output
+// * * * * * 
+// * * * * *
